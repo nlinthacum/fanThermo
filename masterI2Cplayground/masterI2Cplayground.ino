6aa1089e94db62480b91874fb4fe0bc092a52ed0
Nick Linthacum <nlinthacum@zagmail.gonzaga.edu>
2021-02-28 11:41:06 -0800

    learning about I2C

---
+++ masterI2Cplayground/masterI2Cplayground.ino
@@ -0,0 +1,41 @@
+#include<Wire.h>
+
+#define SLAVE_ADDR 9
+
+#define ANSWERSIZE 5
+int buttonPin = 7;
+int buttonState = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin();
+  Serial.begin(9600);
+
+  pinMode(buttonPin, INPUT);
+  
+
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+  delay(50);
+
+  // put your main code here, to run repeatedly:
+  //Send data
+  Wire.beginTransmission(SLAVE_ADDR);
+  Wire.write(buttonState);
+  Wire.endTransmission();
+
+  //Recieve Data
+  //Wire.requestFrom(SLAVE_ADDR, ANSWERSIZE);
+  //String response = "";
+  //while (Wire.available()) {
+   // char ch = Wire.read();
+    //response += ch; 
+  //}
+  //Serial.println(response);
+
+  
+  
+}
---
+++ slaveI2cplayground/slaveI2cplayground.ino
@@ -0,0 +1,42 @@
+#include <Wire.h>
+
+#define SLAVE_ADDR 9
+#define ANSWERSIZE 5
+
+//String answer = "Hello";
+
+void requestEvent();
+void receiveEvent();
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Wire.begin(SLAVE_ADDR); //only pass in when using the slave unit
+
+  Wire.onRequest(requestEvent);
+
+  //Wire.onReceive(receiveEvent);
+
+}
+
+void receiveEvent() {
+  while (0 < Wire.available()) {
+    byte x = Wire.read();
+  }
+}
+
+void requestEvent() {
+  byte response[ANSWERSIZE];
+
+  for (byte i = 0; i <ANSWERSIZE; i++) {
+    response[i] = (byte)answer.charAt(i);
+  }
+  Wire.write(response, sizeof(response));
+}
+  
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(50); 
+}
