1830138d24dd599fb4ec1f39ca33d8ea19f22f3c
Nick Linthacum <nlinthacum@zagmail.gonzaga.edu>
2021-03-11 23:41:49 -0800

    displays temp

--- oled_display/oled_display.ino
+++ oled_display/oled_display.ino
@@ -1,48 +1,63 @@
-/*********
-  Rui Santos
-  Complete project details at https://randomnerdtutorials.com  
-*********/
 
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Fonts/FreeSerif9pt7b.h>
 
+
+#include <DS3232RTC.h>      // https://github.com/JChristensen/DS3232RTC
+DS3232RTC myRTC;
+
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-char Time[]     = "  :  :  ";
-  char Calendar[] = "  /  /20  ";
-  char temperature[] = " 00.00";
-  char temperature_msb;
-  byte i, second, minute, hour, day, date, month, year, temperature_lsb;
+#define OLED_RESET 4
+
+ #define SCREEN_ADDRESS 0x3C 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+
+
 
 void setup(){
-  Serial.begin(115200);
+  Serial.begin(9600);
+  Wire.begin();
+  
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  myRTC.begin();
+
+ display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, SSD1306_WHITE);
+
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(2000);
+  displayRTC();
+  delay(1000);
   
  
 
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
-    Serial.println("SSD1306 allocation failed");
-    for(;;);
-  }
-  delay(2000);
-
-  display.setFont(&FreeSerif9pt7b);
-  display.clearDisplay();
-  display.setTextSize(1);             
-  display.setTextColor(WHITE);        
-  display.setCursor(0,20);             
-  //display.println("Hello, world!");
-  //display.println("Line 2");
- 
- 
-  display.display();
-  delay(2000); 
 }
+
+
+
+
+  
+void loop() {
+ displayRTC();
+  
+}
+
+
 
 void draw_text(byte x_pos, byte y_pos, char *text, byte text_size) {
   display.setCursor(x_pos, y_pos);
@@ -51,67 +66,26 @@ void draw_text(byte x_pos, byte y_pos, char *text, byte text_size) {
   display.display();
 }
 
-void display_temp()
+void displayRTC()
 {
-  if(temperature_msb < 0){
-    temperature_msb = abs(temperature_msb);
-    temperature[0] = '-';
-  }
-  else
-    temperature[0] = ' ';
-  temperature_lsb >>= 6;
-  temperature[2] = temperature_msb % 10  + 48;
-  temperature[1] = temperature_msb / 10  + 48;
-  if(temperature_lsb == 0 || temperature_lsb == 2){
-    temperature[5] = '0';
-    if(temperature_lsb == 0) temperature[4] = '0';
-    else                     temperature[4] = '5';
-  }
-  if(temperature_lsb == 1 || temperature_lsb == 3){
-    temperature[5] = '5';
-    if(temperature_lsb == 1) temperature[4] = '2';
-    else                     temperature[4] = '7';
-  
-}
-draw_text(75, 55, temperature, 1); 
-Serial.println("display temp");
-}
+    char buf[40];
+    time_t t = myRTC.get();
+    float celsius = myRTC.temperature() / 4.0;
+    float fahrenheit = celsius * 9.0 / 5.0 + 32.0;
+    sprintf(buf, "%.2d:%.2d:%.2d %.2d%s%d ",
+        hour(t), minute(t), second(t), day(t), monthShortStr(month(t)), year(t));
+    Serial.print(buf);
+    Serial.print(celsius);
+    Serial.print("C ");
+    Serial.print(fahrenheit);
+    Serial.println("F");
 
+    delay(1000);
+  display.clearDisplay();
 
-
-  
-void loop() {
-   // Write data to DS3231 RTC
-    Wire.beginTransmission(0x68);               // Start I2C protocol with DS3231 address
-    Wire.write(0);                              // Send register address
-    Wire.write(0);                              // Reset sesonds and start oscillator
-    Wire.write(minute);                         // Write minute
-    Wire.write(hour);                           // Write hour
-    Wire.write(day);                            // Write day
-    Wire.write(date);                           // Write date
-    Wire.write(month);                          // Write month
-    Wire.write(year);                           // Write year
-    Wire.endTransmission();                     // Stop transmission and release the I2C bus
-    delay(200);                                 // Wait 200ms
-  
- 
-  Wire.beginTransmission(0x68);                 // Start I2C protocol with DS3231 address
-  Wire.write(0);                                // Send register address
-  Wire.endTransmission(false);                  // I2C restart
-  Wire.requestFrom(0x68, 7);                    // Request 7 bytes from DS3231 and release I2C bus at end of reading
-  second = Wire.read();                         // Read seconds from register 0
-  minute = Wire.read();                         // Read minuts from register 1
-  hour   = Wire.read();                         // Read hour from register 2
-  day    = Wire.read();                         // Read day from register 3
-  date   = Wire.read();                         // Read date from register 4
-  month  = Wire.read();                         // Read month from register 5
-  year   = Wire.read();                         // Read year from register 6
-  Wire.beginTransmission(0x68);                 // Start I2C protocol with DS3231 address
-  Wire.write(0x11);                             // Send register address
-  Wire.endTransmission(false);                  // I2C restart
-  Wire.requestFrom(0x68, 2);                    // Request 2 bytes from DS3231 and release I2C bus at end of reading
-  temperature_msb = Wire.read();                // Read temperature MSB
-  temperature_lsb = Wire.read();                // Read temperature LSB
-  display_temp();
- 
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println(fahrenheit);
+  display.display();
 }
