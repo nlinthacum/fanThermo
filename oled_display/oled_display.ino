0979eaa8ea3ebbe8d7e7131a5368b78f64cf45bb
Nick Linthacum <nlinthacum@zagmail.gonzaga.edu>
2021-03-12 17:23:15 -0800

    buttons added, displays a desired temp

--- oled_display/oled_display.ino
+++ oled_display/oled_display.ino
@@ -5,6 +5,7 @@
 #include <Fonts/FreeSerif9pt7b.h>
 
 
+
 #include <DS3232RTC.h>      // https://github.com/JChristensen/DS3232RTC
 DS3232RTC myRTC;
 
@@ -18,12 +19,25 @@ DS3232RTC myRTC;
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
+#define  button1 8
+#define  button2 9
 
-
+  int button1State;
+  int button2State;
+  float desiredTemp;
+  float currentTemp;
 
 void setup(){
+ 
   Serial.begin(9600);
   Wire.begin();
+
+  pinMode(button1, INPUT);
+  digitalWrite(button1, HIGH);
+
+  pinMode(button2, INPUT);
+  digitalWrite(button2, HIGH);
+
   
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   myRTC.begin();
@@ -40,23 +54,44 @@ void setup(){
   // Show the display buffer on the screen. You MUST call display() after
   // drawing commands to make them visible on screen!
   display.display();
+  display.clearDisplay();
   delay(2000);
   displayRTC();
   delay(1000);
   
  
-
 }
-
-
 
 
   
 void loop() {
  displayRTC();
+ displayDesiredTemp(desiredTemp);
+ 
+ button1State= digitalRead(button1);
+ Serial.println(button1State);
+ button2State= digitalRead(button2);
+ Serial.println(button2State);
+ delay(20);
+ 
+
   
 }
 
+
+
+
+
+void displayDesiredTemp(float desiredTemp)
+{
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+   // display.setCursor(0, 0);
+    display.println("Desired Temp:");
+    display.setTextSize(2);
+    display.println(desiredTemp);
+    display.display();
+}
 
 
 void draw_text(byte x_pos, byte y_pos, char *text, byte text_size) {
@@ -71,21 +106,23 @@ void displayRTC()
     char buf[40];
     time_t t = myRTC.get();
     float celsius = myRTC.temperature() / 4.0;
-    float fahrenheit = celsius * 9.0 / 5.0 + 32.0;
+    currentTemp = celsius * 9.0 / 5.0 + 32.0;
     sprintf(buf, "%.2d:%.2d:%.2d %.2d%s%d ",
         hour(t), minute(t), second(t), day(t), monthShortStr(month(t)), year(t));
-    Serial.print(buf);
-    Serial.print(celsius);
-    Serial.print("C ");
-    Serial.print(fahrenheit);
-    Serial.println("F");
+    //Serial.print(buf);
+    //Serial.print(celsius);
+    //Serial.print("C ");
+    //Serial.print(fahrenheit);
+    //Serial.println("F");
 
     delay(1000);
-  display.clearDisplay();
+    
 
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println(fahrenheit);
-  display.display();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 0);
+    display.println("Current Temp:");
+    display.setTextSize(2);
+    display.println(currentTemp);
+    display.display();
 }
